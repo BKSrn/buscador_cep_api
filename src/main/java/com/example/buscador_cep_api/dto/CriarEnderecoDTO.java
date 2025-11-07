f3b9a30c3ba9e74b9453d65897f14c716cbd4b4f
Bernardo Kist Souza <bekist2006@gmail.com>
2025-11-07 17:32:20 -0300

    update for deploy V2

--- src/main/java/com/example/buscador_cep_api/dto/CriarEnderecoDTO.java
+++ src/main/java/com/example/buscador_cep_api/dto/CriarEnderecoDTO.java
@@ -1,6 +1,6 @@
 package com.example.buscador_cep_api.dto;
 
-import org.antlr.v4.runtime.misc.NotNull;
+import org.jetbrains.annotations.NotNull;
 
-public record CriarEnderecoDTO(@NotNull String cep) {
+public record CriarEnderecoDTO(@org.jetbrains.annotations.NotNull String cep) {
 }
